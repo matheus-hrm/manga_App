0c700b484a8d7cdca912693874044923fc9c5e2e
Matheus Henrique <rodriguesm673@gmail.com>
2023-11-04 20:09:15 -0300

    ✅ updated

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,12 @@ import { hostname } from "os";
 await import("./src/env.mjs");
 const uploads = "uploads.mangadex.org";
 
+module.exports = {
+  typescript: {
+    ignoreBuidErrors: true,
+  }
+}
+
 /** @type {import("next").NextConfig} */
 const config = {
   images: {
