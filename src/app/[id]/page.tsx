655b733f2f1d94f2f860a82ae03c7e732cf61f93
Matheus Henrique <rodriguesm673@gmail.com>
2023-11-04 15:32:36 -0300

    ✅ fixed

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -1,24 +1,28 @@
+import Header from "../components/Header";
+import Footer from "../components/footer";
 
 const BASE_URL = "https://api.mangadex.org"
 
 
 async function data( {params} ) {
   const response = await fetch(`${BASE_URL}/manga/${params.id}`);
-  console.log(response);
   return response.json();
 }
 
-export default async function MangaPage({ params }) {
-  
-  const manga = await data({params});
-  
-  console.log(manga);
 
-  
+export default async function MangaPage({ params }) {
+
+  const manga = await data({ params });
+  console.log(manga); 
   return (
-    <div>
-       
-      
+    <>
+    <Header/>
+    <div className="text-white">
+       <h1>{manga.data.attributes.title.en}</h1>
+        <p>{manga.data.attributes.description.en}</p>
+        <h1>ano:{manga.data.attributes.year}</h1>
     </div>
+    <Footer/>
+    </>
   );
 }  
