7efd2781794f333a830a58c1d3418ef6b71c3355
Rafael Almeida V <104868359+rafaelalmeidaV@users.noreply.github.com>
2023-11-05 11:36:09 -0300

    ✅PageManga/navigationList

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -34,9 +34,9 @@ export default async function MangaPage({ params }) {
     manga.data.attributes.title["ja-ro"] ||
     "sem título";
 
-    const chaptersUnresolved = await fetch(`${BASE_URL}/manga/${params.id}/feed?translatedLanguage[]=pt-br&order[chapter]=desc&limit=500`);
-    const chapters = await chaptersUnresolved.json();
-    console.log(chapters); 
+  const chaptersUnresolved = await fetch(`${BASE_URL}/manga/${params.id}/feed?translatedLanguage[]=pt-br&order[chapter]=desc&limit=500` || `${BASE_URL}/manga/${params.id}/feed?translatedLanguage[]=en&order[chapter]=desc&limit=500`) 
+  const chapters = await chaptersUnresolved.json();
+  console.log(chapters);
   return (
     <>
       <Header />
@@ -45,7 +45,7 @@ export default async function MangaPage({ params }) {
           <img
             src={imageResolved}
             alt={manga.data.attributes.title.en}
-            className="h-auto w-64 rounded-sm"
+            className="h-auto w-64 rounded-sm mt-4"
           />
           <div className="w-1/2 justify-between">
             <div className="mt-4 flex w-[256px] flex-col bg-slate-800">
@@ -60,66 +60,92 @@ export default async function MangaPage({ params }) {
                     {manga.data.attributes.publicationDemographic}
                   </p>
                 </div>
-                <div className="mb-2 flex justify-between">
+                <div className="mb-1 flex justify-between">
                   <p className="text-sm">Ano</p>{" "}
                   <p className="text-sm">{manga.data.attributes.year}</p>
                 </div>
-                <div className="flex justify-between">
-                  <p className="text-sm">Autor</p>{" "}
-                  <p className="text-sm">
-                    {
-                      manga.data.relationships.map(
-                        (item) => item.type === "author",
-                      ).id
-                    }
-                  </p>
-                </div>
-                {/* <p className="text-sm">Gêneros: {manga.data.attributes.tags.map((item) => item.attributes.name["en"]).join(", ")}</p> */}
+
               </div>
             </div>
           </div>
         </div>
-        <div className=" p ml-5 h-[500px] w-[800px] rounded-sm bg-slate-800">
-          <p className="m-4 py-1 text-xl font-extrabold">{title}</p>
-          <p className="mb-4 ml-4 text-base text-slate-500">
-            {manga.data.attributes.altTitles
-              .slice(0, 3)
-              .map((altTitle, index) => (
-                <span key={index}>
-                  {altTitle["ja-ro"] || altTitle["en"] || altTitle["pt-br"]}
-                  {index < 2 ? " " : ""}
-                </span>
-              ))}
-          </p>
+        <div>
+          <div className="ml-5 h-[500px] w-[800px] rounded-sm bg-slate-800">
+            <p className="m-4 py-3 text-xl font-extrabold">{title}</p>
+            <p className="mx-4 text-base text-slate-500">
+              {manga.data.attributes.altTitles
+                .slice(0, 3)
+                .map((altTitle, index) => (
+                  <span key={index}>
+                    {altTitle["ja-ro"] || altTitle["en"] || altTitle["pt-br"]}
+                    {index < 2 ? " " : ""}
+                  </span>
+                ))}
+            </p>
+            <div>
 
-          <p className="m-4 text-base font-bold">Sinopse de {title}</p>
-          <p className="m-4 text-base">
-            {description.split("--")[0].split("**")[0]}
-          </p>
-        </div>
-      </div>
-      <div className="flex px-12 items-center justify-center">
-        <div className="mt-5 bg-slate-800 w-3/4 h-1/2">
-          <div className="flex items-center">
-            <p className="m-4 text-base font-bold">Capítulos</p>
-          </div>
-          {chapters.data.map((chapter) => 
-            (
-              <div key={chapter.id} className="flex items-center overflow-scroll">
-                <p className="m-4 text-base font-bold">
-                  {chapter.attributes.chapter}
-                </p>
-                <p className="m-4 text-base font-bold"> - </p> 
-                <Link href={`${params.id}/${chapter.id}`}>
-                <p className="m-4 text-base font-bold">
-                  {chapter.attributes.title}
-                </p>
-                </Link>
+              <div className="flex flex-wrap mx-2 mt-1">
+                {manga.data.attributes.tags.map((item) => (
+                  <div key={item.id} className="m-2 bg-slate-700 rounded-sm p-2">
+                    <p className="text-sm">{item.attributes.name["pt-br"] || item.attributes.name["en"] || item.attributes.name["ja-ro"]}</p>
+                  </div>
+                ))}
               </div>
-            )
-          )}    
+            </div>
+
+            <p className="m-4 text-base font-bold">Sinopse de {title}</p>
+            <p className="m-4 text-base overflow-y-auto">
+              {description.split("--")[0].split("**")[0]}
+            </p>
+
+          </div>
+
+          <div className="ml-5 w-[800px] rounded-sm">
+            <div className="mt-5 bg-slate-800 max-h-[400px] overflow-y-auto overflow-x-hidden" style={{ overflow: 'scroll', scrollbarWidth: 'thin', overflowX: 'hidden' }}>
+              <style>
+
+                {`
+       
+                  ::-webkit-scrollbar {
+                    width: 10px;
+                  }
+        
+                  ::-webkit-scrollbar-thumb {
+                    background: #888;
+                  }
+
+        
+                  ::-webkit-scrollbar-thumb:hover {
+                    background: #555;
+                  }
+                `}
+
+              </style>
+              <div className="flex items-center">
+                <p className="m-4 text-base font-bold">Capítulos de {title}</p>
+              </div>
+              {chapters.data.map((chapter) => (
+                <div key={chapter.id} className="flex items-center border-[1px] mx-4 my-2 rounded-md ">
+                  <Link href={`${params.id}/${chapter.id}`}>
+                    <p className="mx-2 mt-2 text-base hover:text-[#776be0]">
+                      Capítulo {chapter.attributes.chapter}
+                    </p>
+                    <p className="mx-2 mb-2 font-light text-xs">
+                      {chapter.attributes.title}
+                    </p>
+                  </Link>
+                </div>
+              ))}
+            </div>
+          </div>
+
+
         </div>
+
       </div>
+
+
+
 
       <Footer />
     </>
--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -4,7 +4,7 @@ import NavBar from './NavBar'
 
 const Header = () => {
   return (
-    <header className="bg-[#03001A] p-8">
+    <header className="bg-[rgb(3,0,26)] p-8">
         <div className="container mx-auto flex justify-between items-center">
             <Link href="/" className="sm:text-2xl md:text-4xl font-semibold text-white">mangás</Link>
             <NavBar/>
