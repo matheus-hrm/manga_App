e9a5565a08d2d07a41d79ee59993a1a4aa2df4a1
Matheus Henrique <rodriguesm673@gmail.com>
2023-11-17 13:12:56 -0300

    create loading component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import { MangaList } from './components/MangaList';
 import Footer from './components/footer';
 import Header from './components/Header';
 import { Suspense } from 'react';
+import { HomeSkeleton } from './components/HomeSkeleton';
 
 const rating = {
   order: {
@@ -18,34 +19,7 @@ const seguidos = {
   limit: 10
 }
 
-export function HomeSkeleton() {
-  return (
-    <>
-    <div className="flex flex-wrap items-center px-5 py-3 sm:flex-wrap xl:flex-nowrap ">
-    {Array(10).fill(0).map((_, index) => (
-      <div
-        key={index}
-        className="group relative m-2 flex max-w-xs flex-col items-center sm:max-w-sm md:max-w-md lg:max-w-lg  animate-pulse bg-gray-400"
-      >
-        <div className="relative min-h-fit overflow-hidden h-36 w-24 sm:h-48 sm:w-32 md:h-60 md:w-40">
-        </div>
-      </div>
-    ))}
-  </div>
-  <div className="flex flex-wrap items-center px-5 py-3 sm:flex-wrap xl:flex-nowrap ">
-    {Array(10).fill(0).map((_, index) => (
-      <div
-        key={index}
-        className="group relative m-2 flex max-w-xs flex-col items-center sm:max-w-sm md:max-w-md lg:max-w-lg  animate-pulse bg-gray-400"
-      >
-        <div className="relative min-h-fit overflow-hidden h-36 w-24 sm:h-48 sm:w-32 md:h-60 md:w-40 ">
-        </div>
-      </div>
-    ))}
-  </div>
-  </>
-  )
-}
+
 
 export default function Home() {
 
