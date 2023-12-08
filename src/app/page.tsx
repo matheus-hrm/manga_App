57d8c7ae28cdf88bb49a81eb23a70c4568ec69b1
Matheus Henrique <rodriguesm673@gmail.com>
2023-12-08 19:59:25 -0300

    init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import Link from 'next/link'
 import { MangaList } from './components/MangaList';
-import Footer from './components/footer';
+import Footer from './components/footer'
 import Header from './components/Header';
 import { Suspense } from 'react';
 import { HomeSkeleton } from './components/HomeSkeleton';
+import { HomeCarousel } from './components/HomeCarousel';
 
 const rating = {
   order: {
@@ -27,13 +27,15 @@ export default function Home() {
     <>
       <div className="text-white h-screen w-screen overflow-scroll">
         <Header />
+        <div className='flex items-center justify-center'>
+        <HomeCarousel />
+        </div>
         <Suspense fallback={<HomeSkeleton />}>
           <h1 className='text-2xl p-5 ml-6 font-thin'>Mangás em destaque</h1>
           <MangaList params={seguidos}/>
           <h1 className='text-2xl p-5 ml-6 font-thin'>Mais bem avaliados</h1>
           <MangaList params={rating} />
         </Suspense>
-        <Link href="/login" className='p-4 '>Login</Link>
         <div className='bottom-0'>
           <Footer />
         </div>
